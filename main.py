11cda271adafe78221d79c3c84798684a604a73e
abnurkan-dev <abnurkan1997@gmail.com>
2025-10-20 14:24:58 +0100

    main updated

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ async def root():
     return {"message": "Welcome to the String Analyzer Service API"}
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-        print("➡️ Proceeding without rate limiting.")
-
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.getenv("PORT", 8000))  # PXXL sets this automatically
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
